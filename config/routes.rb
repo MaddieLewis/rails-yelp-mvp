c364b35b8868371c2e449304310acfea01069bec
Maddie Lewis <maddie@simplywellbeing.com>
2019-02-20 16:43:56 +0000

    Challenge complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants do
-    collection do
-    end
-    member do
-    end
+    # collection do
+    # end
+    # member do
+    # end
     resources :reviews, only: %i[new create]
   end
   resources :reviews, only: %i[show edit update destroy]
